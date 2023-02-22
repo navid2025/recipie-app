74c8e8f2a11639be678490e72d84968071195868
n.zare <n.zare@behsacorp.com>
2023-02-22 16:40:25 +0330

    git commit -m "ExceptionHandling"

--- src/main/java/com/navid/recipieapp/controller/RecipeController.java
+++ src/main/java/com/navid/recipieapp/controller/RecipeController.java
@@ -1,11 +1,17 @@
 package com.navid.recipieapp.controller;
 
+import com.navid.recipieapp.domain.Recipe;
+import com.navid.recipieapp.exception.NotFoundException;
 import com.navid.recipieapp.repository.RecipeRepository;
 import com.navid.recipieapp.service.RecipeService;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author n.zare 2/8/2023 $
@@ -25,9 +31,35 @@ public class RecipeController {
   model.addAttribute("recipes", recipeService.getRecipes());
   return "recipe/index";
  }
- @RequestMapping("recipe/find/{id}")
+ @RequestMapping("/recipe/find/{id}")
  public String getRecipeById(@PathVariable String id, Model model) {
-  model.addAttribute("recipe", recipeService.getRecipeById(Long.parseLong(id)));
+  if(id instanceof String){
+   throw new NumberFormatException();
+  }
+
+  Recipe recipe = recipeService.getRecipeById(Long.valueOf(id));
+  if (recipe != null) {
+
+   model.addAttribute("recipe", recipe);
+  }
+  else throw new NotFoundException("NOT FOUND");
+
   return "recipe/show";
  }
+
+ @ResponseStatus(HttpStatus.NOT_FOUND)
+ @ExceptionHandler(NotFoundException.class)
+ public ModelAndView notFoundException(){
+  ModelAndView mv = new ModelAndView();
+  mv.setViewName("exception/not-found-exception");
+  return mv;
+ }
+ @ResponseStatus(HttpStatus.BAD_REQUEST)
+ @ExceptionHandler(NumberFormatException.class)
+ public ModelAndView numberFormatExceptionHandler(){
+  ModelAndView mv = new ModelAndView();
+  mv.setViewName("exception/number-format-exception");
+  return mv;
+ }
+
 }
---
+++ src/main/java/com/navid/recipieapp/exception/NotFoundException.java
@@ -0,0 +1,27 @@
+package com.navid.recipieapp.exception;
+
+/**
+ * @author n.zare 2/22/2023 $
+ */
+
+
+public class NotFoundException extends RuntimeException{
+ public NotFoundException() {
+ }
+
+ public NotFoundException(String message) {
+  super(message);
+ }
+
+ public NotFoundException(String message, Throwable cause) {
+  super(message, cause);
+ }
+
+ public NotFoundException(Throwable cause) {
+  super(cause);
+ }
+
+ public NotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+  super(message, cause, enableSuppression, writableStackTrace);
+ }
+}
